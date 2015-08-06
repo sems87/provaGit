dd14904c8f565b2331656be6ff579c6fc28b595a
Salvatore <sems1987@gmail.com>
2015-08-06 11:10:39 +0200

    committed

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'prova modifica github' });
+  res.render('index', { title: 'prova modifica github 2' });
 });
 
 module.exports = router;
